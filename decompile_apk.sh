2fdb57791790d516189618ef17715044a1be0a23
pangliang <418094911@qq.com>
2015-05-06 09:25:09 +0800

    升级apktool 到2.0.0

--- decompile_apk.sh
+++ decompile_apk.sh
@@ -7,5 +7,5 @@ APK=$1
 DECOMPILE_DIR=`basename $APK .apk`
 JAR=$DECOMPILE_DIR/source.jar
 
-java -jar $HOME/tools/apktool.jar d -f $APK  $DECOMPILE_DIR
+java -jar $HOME/tools/apktool_2.0.0.jar d -f $APK
 $HOME/tools/dex2jar-0.0.9.15/d2j-dex2jar.sh --force $APK -o $JAR
