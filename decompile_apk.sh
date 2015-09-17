21742d13e1d16797ba071f3b23f6f89ce10d9b00
pangliang <418094911@qq.com>
2015-09-17 19:54:55 +0800

    更新dex2jar 到2.1,  更新smali 到2.0.6

--- decompile_apk.sh
+++ decompile_apk.sh
@@ -8,4 +8,4 @@ DECOMPILE_DIR=`basename $APK .apk`
 JAR=$DECOMPILE_DIR/source.jar
 
 java -jar $HOME/tools/apktool_2.0.0.jar d -f $APK
-$HOME/tools/dex2jar-0.0.9.15/d2j-dex2jar.sh --force $APK -o $JAR
+$HOME/tools/dex2jar-2.1-SNAPSHOT/d2j-dex2jar.sh --force $APK -o $JAR
